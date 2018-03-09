5e3f9f21fa1f11c90b3ede0eaf4460c1986bb677
johny <przemek.dwornikowski@wp.pl>
2018-03-09 19:06:41 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+float x, a, b, c, g;
+
+
+
+	cout<<"podaj liczbe"<<endl;
+	cin>>x;
+
+
+
+	a=pow(x, 0.125);
+
+	b=(2*(sqrt(pow(x, 2) +1))* abs(x)) / (sin (x*(pow(x,2))));
+
+	c=((pow(pow(x,2)+abs(x),0.25))/(pow(x,-1.6)*pow(tan(x),2))) * M_E * exp(x);
+
+	cout<<a<<endl<<b<<endl<<c<<endl;
+
+    return 0;
+
+}
